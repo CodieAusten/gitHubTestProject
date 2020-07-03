8f608187aeb1dda56a949b03370314f702ea59f1
jack <austencodie@gmail.com>
2020-07-02 19:14:35 -0500

    Initial Commit to GitHub

---
+++ gitHubTestProject/src/gitHubTestProject/helloGitHub.java
@@ -0,0 +1,11 @@
+package gitHubTestProject;
+
+public class helloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub");
+
+	}
+
+}
